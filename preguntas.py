a0d68b7682f6362fe13ef4a6f5f6a7d1959ff67b
Dario Bravo <127920216+djbravoc@users.noreply.github.com>
2023-04-15 15:40:20 -0500

    Update preguntas.py

--- preguntas.py
+++ preguntas.py
@@ -11,7 +11,10 @@ Utilice el archivo `data.csv` para resolver las preguntas.
 
 
 """
-
+from csv import reader
+file = open("data.csv","r")
+archivo = reader(file,delimiter= "\t")
+archivo = list(archivo)
 
 def pregunta_01():
     """
